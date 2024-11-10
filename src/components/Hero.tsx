5a002aecc4f9fddba65e5e1f49c1f49a93dce557
Tahabaig044 <baigtaha07@gmail.com>
2024-11-10 02:39:17 -0800

    portfolio-custom-css

---
+++ src/components/Hero.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./Navbar";
+import '../app/styles/hero.css';
+
+const Hero = () => {
+  return (
+    <div
+      id="hero"
+      className="hero-container"
+    >
+      <Navbar />
+      <div className="hero-content">
+        <div></div>
+
+        <div className="hero-text">
+          <div className="hero-msin"> 
+            <p data-aos="zoom-in-up">I am</p>
+            <p data-aos="zoom-in-up">Taha Baig</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
